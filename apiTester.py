fe2063c4fb72f36ce18e75a920decfc471a80754
unknown <faisaljfcl@gmail.com>
2022-11-08 00:51:48 +0600

    POST comments implemented.

--- apiTester.py
+++ apiTester.py
@@ -4,6 +4,7 @@ import re
 URL = "http://localhost:3001"
 #QUERY = "?uid=something_something"
 
+# service 1 _id 63694a76069a6e37d0afa78a
 '''
 def GET_TOKEN():
     header = { "Content-Type":"application/json", "uid":"something_something"   }
@@ -22,6 +23,16 @@ def GET():
     url = URL + "/services"
     res = rq.get(url, headers=header)
     result = res.json()
+    for data in result["data"]:
+        print(data["title"])
+
+def POST():
+    header = { "Content-Type":"application/json", "uid":"someUsersUid", "service_id":"63694a76069a6e37d0afa78a"}
+    body ={ "review":"Nice experience. No pain. Didn't feel any trouble"  }
+    url = URL + "/comments"
+    res = rq.post(url, headers=header, json=body)
+    result = res.json()
     print(result)
 
-GET()
+
+POST()
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // Import Packages
 const express = require("express");
 const mongodb = require("mongodb");
+const cors = require("cors");
 //-----------------------------------------
 
 // -------------------- Initializations --------------------
 require("dotenv").config(); // dotenv
 const app = express(); // express
 const { MongoClient, ObjectId } = mongodb;
+//-----------------------------------------
+
+// Middleware options
+const corsOptions = {
+  origin: process.env.CLIENT_ADDRESS || process.env.DEV_CLIENT,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  withCredentials: true,
+};
+// Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
 //-----------------------------------------
 
 //------------------- Accessing Secrets --------------------
@@ -18,6 +30,7 @@ const { DB_URI, DB_NAME } = process.env;
 
 // Collections
 let servicesCollection;
+let commentsCollection;
 //--------------------
 
 MongoClient.connect(DB_URI, function (err, client) {
@@ -27,12 +40,17 @@ MongoClient.connect(DB_URI, function (err, client) {
   }
 
   servicesCollection = client.db(DB_NAME).collection("services");
+  commentsCollection = client.db(DB_NAME).collection("comments");
   console.log("DB CONNECTED");
 });
 
 //-----------------------------------------
 
 // --------------- API END POINTS / Controllers ---------
+
+// Handling GET requests ------------------
+
+/* Get services */
 app.get("/services", async (req, res) => {
   try {
     const query = {};
@@ -48,6 +66,36 @@ app.get("/services", async (req, res) => {
     res.status(501).send({ error: true, message: "Query Failed" });
   }
 });
+// END of Handling GET requests ------------------
+
+// Handling POST requests
+
+/* Post comments */
+app.post("/comments", async (req, res) => {
+  try {
+    const { uid, service_id } = req.headers;
+
+    /*
+     * // Body Interface
+     * body: {
+     *	review: String,
+     *
+     * }
+     *
+     */
+
+    const body = req.body;
+    const load = { ...body, uid, service_id: ObjectId(service_id) };
+
+    const response = await commentsCollection.insertOne(load);
+    response["error"] = false;
+    res.status(200).send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(501).send({ error: true, message: "COMMENT POST FAILED!!" });
+  }
+});
+// END of Handling POST requests ------------------
 
 // Listening to PORT
 app.listen(PORT, () => console.log(`SERVER is running at port: ${PORT}`));
