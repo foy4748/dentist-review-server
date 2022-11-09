c004eee32abc75aa6a95e500e5a053c4119db35c
unknown <faisaljfcl@gmail.com>
2022-11-10 00:28:39 +0600

    Added CORS policy for deployed client to prevent script attack

--- index.js
+++ index.js
@@ -12,17 +12,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = mongodb;
 //-----------------------------------------
 
 // Middleware options
-/*
 const corsOptions = {
-  origin: process.env.CLIENT_ADDRESS || process.env.DEV_CLIENT,
+  origin: [process.env.CLIENT_ADDRESS, process.env.DEV_CLIENT],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   withCredentials: true,
 };
-*/
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 //-----------------------------------------
 
 //------------------- Accessing Secrets --------------------
